370361818f8c8f6f44a28a9e4f3a73be64ccc8d5
Nicolas Alvarez <nicoalvarez9915@gmail.com>
2020-07-26 23:37:01 -0500

    Holaaaa

--- introduccion/ejercicio_1.Designer.vb
+++ introduccion/ejercicio_1.Designer.vb
@@ -94,7 +94,7 @@ Partial Class ejercico_1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(254, 200)
+        Me.Button1.Location = New System.Drawing.Point(234, 200)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 14
